6f850e78913c9054215cdff80e36669bc915303d
MedDhiia <Med.Dhiia@Gmail.com>
2020-11-29 20:43:58 +0100

    Create EventSubscriber to setting author automatically from the Token

--- src/Entity/BlogPost.php
+++ src/Entity/BlogPost.php
@@ -55,7 +55,6 @@ class BlogPost
     /**
      * @ORM\ManyToOne(targetEntity="App\Entity\User", inversedBy="posts")
      * @ORM\JoinColumn(nullable=false)
-     * @Assert\NotBlank()
      */
     private $author ;
 
