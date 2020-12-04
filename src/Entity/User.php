a0f6026a65f51e1ceb525161bf0d6f29cd78c4de
MedDhiia <Med.Dhiia@Gmail.com>
2020-12-04 23:22:10 +0100

    Make Admin see all Users emails

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -104,7 +104,7 @@ class User implements UserInterface
 
     /**
      * @ORM\Column(type="string", length=255)
-     * @Groups({"put", "post"})
+     * @Groups({"put", "post", "get-admin"})
      * @Assert\NotBlank()
      * @Assert\Email()
      * @Assert\Length(min=6, max=255)
@@ -125,6 +125,7 @@ class User implements UserInterface
 
     /**
      * @ORM\Column(type="simple_array", length=200)
+     * @Groups({"get-admin"})
      */
     private $roles = [];
 
